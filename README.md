0d47681a773cad7683a646608cda2b1411daa317
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-12-18 09:44:39 -0300

    make the main structure

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# youtubeWatcher
 a webApp to watch youtube videos in a separate windows without getting distracted by the youtube algorithm
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import "react"
+import "react-dom"
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.querySelector("main"))
+root.render(
+
+  <>
+  </>
+)
